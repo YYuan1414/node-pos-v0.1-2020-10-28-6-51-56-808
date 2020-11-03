a3b750c89a2946ae381170f761b6b071364bc1f3
YYuan1414 <73294738+YYuan1414@users.noreply.github.com>
2020-11-03 18:10:49 +0800

    backend_test

---
+++ ToDoItemApi-2020-11-3-6-40-30-108/ToDoItemApi/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using ToDoItemApi.Model;
+using ToDoItemApi.Model.Configuration;
+using ToDoItemApi.Services;
+
+namespace ToDoItemApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoListController : ControllerBase
+
+    {
+        private IConfiguration _configuration;
+        private IToDoListService _toDoListService1;
+
+        public ToDoListController(IConfiguration configuration, IToDoListService toDoListService)
+        {
+            _configuration = configuration;
+            _toDoListService1 = toDoListService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ToDoItem>>> GetToDoItems()
+        {
+            var items = _toDoListService1.GetToDoItems();
+            return Ok(items);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItemById(long id)
+        {
+            var item = _toDoListService1.GetToDoItemById(id);
+            if (item == null)
+            {
+                return NotFound($"No item match id: {id}");
+            }
+
+            return Ok(item);
+        }
+    }
+}
---
+++ ToDoItemApi-2020-11-3-6-40-30-108/ToDoItemApi/Models/Configuration/GetOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoItemApi.Model.Configuration
+{
+    public sealed class GetOptions
+    {
+        public bool CaseSensitive { get; set; }
+
+        [Required]
+        public string FilterCriteria { get; set; }
+
+        //public PagingOptions PagingOptions { get; set; }
+    }
+}
